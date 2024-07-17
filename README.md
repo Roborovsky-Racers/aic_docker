1fb61975c5bd6c5900ca361ee63fdd74dfb5d04e
Takashi Sato <t.sato17123@gmail.com>
2024-07-17 09:22:20 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# aic_docker
+
+## initial setup
+```
+curl -O https://raw.githubusercontent.com/Roborovsky-Racers/aic_docker/main/scripts/setup.sh
+bash ./aic_docker.bash
+rm ./setup.bash
+```
+OR
+```
+mkdir -p ~/aic/ && cd ~/aic/
+git clone https://github.com/Roborovsky-Racers/aic_docker.git
+./scripts/setup.bash
+```
+
+## build docker image
+```
+cd ~/aic/aic_docker
+make build
+```
+
+## up docker container from built image (without GPU)
+```
+cd ~/aic/aic_docker
+make up
+```
+
+## up docker container from built image (with NVIDIA GPU)
+```
+cd ~/aic/aic_docker
+make gpu-up
+```
---
+++ scripts/setup.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DEFAULT_AIC_HOME="${HOME}/aic"
+if [ -z "${AIC_HOME}" ]; then
+  echo "Warn: Environment variable 'AIC_HOME' is not set."
+
+  read -p "Do you want to use the default value '$DEFAULT_AIC_HOME'? (y/n): " choice
+
+  if [ "$choice" = "y" ]; then
+    AIC_HOME=$DEFAULT_AIC_HOME
+    echo "Using default value: ${AIC_HOME}"
+  else
+    echo "Aborted."
+    exit 1
+  fi
+fi
+echo "'AIC_HOME' is set to '${AIC_HOME}'"
+
+# clone aic_install if it does not exist
+if [ ! -d "${AIC_HOME}/aic_install" ]; then
+  git clone git@github.com:Roborovsky-Racers/aic_install.git ${AIC_HOME}/aic_install
+fi
+
+cd ${AIC_HOME}
+git clone -b develop git@github.com:Roborovsky-Racers/aichallenge-2024.git ${AIC_HOME}/aichallenge-2024
+git clone git@github.com:AutomotiveAIChallenge/autoware-practice.git  ${AIC_HOME}/autoware-practice
