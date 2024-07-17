e455c08655ad3159a6b68eb1fcd561beea214fff
Takashi Sato <t.sato17123@gmail.com>
2024-07-17 22:22:40 +0900

    add download AWSIM

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -21,6 +21,18 @@ if [ ! -d "${AIC_HOME}/aic_docker" ]; then
   git clone git@github.com:Roborovsky-Racers/aic_docker.git ${AIC_HOME}/aic_docker
 fi
 
+# clone aichallenge-2024 and autoware-practice
 cd ${AIC_HOME}
 git clone -b develop git@github.com:Roborovsky-Racers/aichallenge-2024.git ${AIC_HOME}/aichallenge-2024
 git clone git@github.com:AutomotiveAIChallenge/autoware-practice.git  ${AIC_HOME}/autoware-practice
+
+# download AWSIM and AWSIM_GPU
+AWSIM_FILE_ID="1OwdYrEnyX68CzzdBl9nj7oCxVsQkO4UJ";
+AWSIM_GPU_FILE_ID="1rOhjb63UTz9SCl_l6mpx-6igPE8-XGPL";
+curl -L "https://drive.usercontent.google.com/download?id=${AWSIM_FILE_ID}&export=download&confirm=y" -o AWSIM.zip
+unzip AWSIM.zip &
+curl -L "https://drive.usercontent.google.com/download?id=${AWSIM_GPU_FILE_ID}&export=download&confirm=y" -o AWSIM_GPU.zip
+unzip AWSIM_GPU.zip
+mv AWSIM ${AIC_HOME}/aichallenge-2024/aichallenge/simulator/
+mv AWSIM_GPU ${AIC_HOME}/aichallenge-2024/aichallenge/simulator/
+rm -rf AWSIM.zip AWSIM_GPU.zip
